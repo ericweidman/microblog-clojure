528f3887b4ce8e38acd9c5ddc804d3ad3d43d201
ericweidman <ericweidman@gmail.com>
2016-03-24 11:37:57 -0400

    Added post route

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -1,8 +1,13 @@
 (ns microblog-clojure.core
   (:require [ring.adapter.jetty :as j]
             [compojure.core :as c]
-            [hiccup.core :as h])
+            [hiccup.core :as h]
+            [ring.middleware.params :as p]
+            [ring.util.response :as r])
   (:gen-class))
+
+(defonce messages (atom[]))
+(defonce server (atom nil))
 
 (c/defroutes app
   (c/GET "/" request
@@ -10,10 +15,18 @@
             [:body
              [:form {:action "/add-message" :method "post"}
               [:input {:type "text" :placeholder "Enter Message" :name "message"}]
-              [:button {:type "Submit"} "Add Message"]]]])))
-(defonce server (atom nil))
+              [:button {:type "Submit"} "Add Message"]]
+             [:ul
+              (map (fn [message]
+                     [:li message])
+                @messages)]]]))
+
+  (c/POST "/add-message" request
+    (let [message (get (:params request) "message")]
+      (swap! messages conj message)
+      (r/redirect "/"))))
 
 (defn -main []
   (when @server
     (.stop @server))
-  (reset! server (j/run-jetty app {:port 8080 :join? false})))
+  (reset! server (j/run-jetty (p/wrap-params app) {:port 8080 :join? false})))
